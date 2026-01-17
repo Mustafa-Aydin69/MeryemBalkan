3f3c234a076c65af6f160363fdc98153d5156ddb
Mustafa-Aydin69 <mustafamulti12@gmail.com>
2026-01-17 16:10:50 +0300

    eslint hatası giderildi.

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true, // ✅ Netlify/Render için gerekli
   },
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ Build sırasında ESLint hatalarını görmezden gel
-  },
   typescript: {
     ignoreBuildErrors: true, // (Opsiyonel) TS hataları yüzünden build fail olmasın
   },
