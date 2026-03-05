b6e2531be84249f45ec364086289173c79d21c6c
Mustafa-Aydin69 <mustafamulti12@gmail.com>
2026-03-05 23:14:04 +0300

    NextConfig

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,15 @@ const nextConfig: NextConfig = {
   },
   // iyzipay: fs.readdirSync + dynamic require kullanıyor, bundle'da çözülmüyor
   serverExternalPackages: ['iyzipay'],
+  // Vercel: iyzipay lib/resources runtime'da scandir ile yüklüyor; trace'a tam paketi ekle
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/payment/bin-check': ['./node_modules/iyzipay/**/*'],
+      '/api/payment/3ds-init': ['./node_modules/iyzipay/**/*'],
+      '/api/payment/3ds-callback': ['./node_modules/iyzipay/**/*'],
+      '/api/payment/webhook': ['./node_modules/iyzipay/**/*'],
+    },
+  } as NextConfig['experimental'],
 };
 
 export default nextConfig;
