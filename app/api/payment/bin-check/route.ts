05ff039d62e6f498ed70000ac5386c0f63ff909f
Mustafa-Aydin69 <mustafamulti12@gmail.com>
2026-03-06 22:44:20 +0300

    default kaldırıldı

--- app/api/payment/bin-check/route.ts
+++ app/api/payment/bin-check/route.ts
@@ -54,7 +54,7 @@ export async function OPTIONS() {
   });
 }
 
-export default async function POST(req: NextRequest) {
+export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
