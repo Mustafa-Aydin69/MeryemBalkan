918f993ac3db75e00abab588199f3752f8a4834f
MustafaAydin <mustafamulti12@gmail.com>
2026-06-11 23:19:02 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -92,7 +92,7 @@ const nextConfig: NextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline'",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval'",
               "style-src 'self' 'unsafe-inline' https://cdnjs.cloudflare.com",
               "img-src 'self' data: blob: https://cdn.meryembalkan.com.tr https://www.meryembalkan.com.tr https://*.supabase.co",
               "media-src 'self' https://cdn.meryembalkan.com.tr",
